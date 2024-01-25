92b2316c4110f0e3085c34f1e43821de736c3206
gabrielrportugal <gabrielrportugal@outlook.com>
2024-01-25 12:06:27 -0300

    feat: jwtstrategy implementation

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt'
+import { Env } from 'src/env'
+import { z } from 'zod'
+
+const tokenPayloadSchema = z.object({
+  sub: z.string().uuid(),
+})
+
+export type UserPayload = z.infer<typeof tokenPayloadSchema>
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService<Env, true>) {
+    const publicKeyBase64 = config.get('JWT_PUBLIC_KEY')
+
+    const options: StrategyOptions = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      algorithms: ['RS256'],
+      secretOrKey: Buffer.from(publicKeyBase64, 'base64'),
+    }
+
+    super(options)
+  }
+
+  async validate(payload: UserPayload) {
+    return tokenPayloadSchema.parse(payload)
+  }
+}
