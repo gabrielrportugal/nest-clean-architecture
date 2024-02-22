1bcddbef339fee0b87af16a395d42eb281adc459
gabrielrportugal <gabrielrportugal@outlook.com>
2024-02-22 17:48:16 -0300

    refactor: authenticate controller calling authentica use case

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -12,6 +12,8 @@ import { QuestionAttachmentsRepository } from '@/domain/forum/application/reposi
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   providers: [
@@ -40,6 +42,10 @@ import { AnswerAttachmentsRepository } from '@/domain/forum/application/reposito
       provide: QuestionsRepository,
       useClass: PrismaQuestionsRepository,
     },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
+    },
   ],
   exports: [
     PrismaService,
@@ -49,6 +55,7 @@ import { AnswerAttachmentsRepository } from '@/domain/forum/application/reposito
     QuestionAttachmentsRepository,
     QuestionCommentsRepository,
     QuestionsRepository,
+    StudentsRepository,
   ],
 })
 export class DatabaseModule {}
