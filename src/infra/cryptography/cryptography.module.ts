af6e278c551520920073f76a48588824ab493f29
gabrielrportugal <gabrielrportugal@outlook.com>
2024-02-22 17:32:54 -0300

    feat: implements cryptography classes

---
+++ src/infra/cryptography/cryptography.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common'
+
+import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+
+import { BcryptHasher } from './bcrypt-hasher'
+import { JwtEncrypter } from './jwt-encrypter'
+
+@Module({
+  providers: [
+    {
+      provide: Encrypter,
+      useClass: JwtEncrypter,
+    },
+    {
+      provide: HashGenerator,
+      useClass: BcryptHasher,
+    },
+    {
+      provide: HashComparer,
+      useClass: BcryptHasher,
+    },
+  ],
+  exports: [Encrypter, HashGenerator, HashComparer],
+})
+export class CryptographyModule {}
