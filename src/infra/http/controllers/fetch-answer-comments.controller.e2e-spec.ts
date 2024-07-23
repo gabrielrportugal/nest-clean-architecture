badb5522069de88bb46a00d9f4dab16464fa6c26
gabrielrportugal <gabrielrportugal@outlook.com>
2024-07-23 15:54:11 -0300

    feat: fetch answer comments controller

---
+++ src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
@@ -0,0 +1,91 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { AnswerFactory } from 'test/factories/make-answer'
+import { AnswerCommentFactory } from 'test/factories/make-answer-comment'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('[Fetch Answer Comments (E2E)]', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+  let answerCommentFactory: AnswerCommentFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AnswerFactory,
+        AnswerCommentFactory,
+      ],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    answerCommentFactory = moduleRef.get(AnswerCommentFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET] /answers/:answerId/comments', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const answer = await answerFactory.makePrismaAnswer({
+      authorId: user.id,
+      questionId: question.id,
+    })
+
+    await Promise.all([
+      await answerCommentFactory.makePrismaAnswerComment({
+        authorId: user.id,
+        answerId: answer.id,
+        content: 'Answer Comment 1',
+      }),
+      await answerCommentFactory.makePrismaAnswerComment({
+        authorId: user.id,
+        answerId: answer.id,
+        content: 'Answer Comment 2',
+      }),
+      await answerCommentFactory.makePrismaAnswerComment({
+        authorId: user.id,
+        answerId: answer.id,
+        content: 'Answer Comment 3',
+      }),
+    ])
+
+    const answerId = answer.id.toString()
+
+    const response = await request(app.getHttpServer())
+      .get(`/answers/${answerId}/comments`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+
+    expect(response.body).toMatchObject({
+      comments: expect.arrayContaining([
+        expect.objectContaining({ content: 'Answer Comment 1' }),
+        expect.objectContaining({ content: 'Answer Comment 2' }),
+        expect.objectContaining({ content: 'Answer Comment 3' }),
+      ]),
+    })
+  })
+})
---
+++ src/infra/http/controllers/fetch-answer-comments.controller.ts
@@ -0,0 +1,46 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { CommentPresenter } from '../presenters/comment-presenter'
+import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1).default(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+type QueryParamsSchema = z.infer<typeof pageQueryParamSchema>
+
+@Controller('/answers/:answerId/comments')
+export class FetchAnswerCommentsController {
+  constructor(private fetchAnswerCommentsUC: FetchAnswerCommentsUseCase) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: QueryParamsSchema,
+    @Param('answerId') answerId: string,
+  ) {
+    const result = await this.fetchAnswerCommentsUC.execute({
+      page,
+      answerId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    const { answerComments } = result.value
+
+    return { comments: answerComments.map(CommentPresenter.toHttp) }
+  }
+}
